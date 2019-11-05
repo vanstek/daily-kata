5e25438ae96151ed4be77f92daec522b1c8927f7
Alex van Stekelenborg <poesidom@gmail.com>
2019-11-05 16:48:00 -0500

    Update Growth of a Population.py

--- Growth of a Population.py
+++ Growth of a Population.py
@@ -1,3 +1,11 @@
+#Growth of a Population
+#Level: 7 kyu
+'''
+In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year 
+and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or 
+equal to p = 1200 inhabitants?
+'''
+
 def nb_year(p0, percent, aug, p):
     n = 0
     percent = 1 + percent/100    
