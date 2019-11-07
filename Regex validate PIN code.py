6b3660e707ec680cea3f9243dbc59162e4b46be5
Alex van Stekelenborg <poesidom@gmail.com>
2019-11-06 23:12:40 -0500

    Create Regex validate PIN code.py

---
+++ Regex validate PIN code.py
@@ -0,0 +1,11 @@
+#Regex validate PIN code
+#Level: 7 kyu
+'''
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+'''
+
+import re
+def validate_pin(pin):
+  return bool(re.match(r^'\d{4}|\d{6}),pin)
