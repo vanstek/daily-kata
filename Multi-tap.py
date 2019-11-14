e48daa4457c8ea051dd063cd0a69bc51aae1d299
Alex van Stekelenborg <poesidom@gmail.com>
2019-11-13 21:12:51 -0500

    Create Multi-tap.py

---
+++ Multi-tap.py
@@ -0,0 +1,33 @@
+#Multi-tap Keypad Text Entry on an Old Mobile Phone
+'''
+Prior to the development of T9 (predictive text entry) systems, the method to type words was called "multi-tap" and involved pressing a button repeatedly to cycle through the possible values.
+
+For example, to type a letter "R" you would press the 7 key three times (as the screen display for the current character cycles through P->Q->R->S->7). A character is "locked in" once the user presses a different key or pauses for a short period of time (thus, no extra button presses are required beyond what is needed for each letter individually). The zero key handles spaces, with one press of the key producing a space and two presses producing a zero.
+
+In order to send the message "WHERE DO U WANT 2 MEET L8R" a teen would have to actually do 47 button presses. No wonder they abbreviated.
+
+For this assignment, write a module that can calculate the amount of button presses required for any phrase. Punctuation can be ignored for this exercise. Likewise, you can assume the phone doesn't distinguish between upper/lowercase characters (but you should allow your module to accept input in either for convenience).
+Prior to the development of T9 (predictive text entry) systems, the method to type words was called "multi-tap" and involved pressing a button repeatedly to cycle through the possible values.
+
+For example, to type a letter "R" you would press the 7 key three times (as the screen display for the current character cycles through P->Q->R->S->7). A character is "locked in" once the user presses a different key or pauses for a short period of time (thus, no extra button presses are required beyond what is needed for each letter individually). The zero key handles spaces, with one press of the key producing a space and two presses producing a zero.
+
+In order to send the message "WHERE DO U WANT 2 MEET L8R" a teen would have to actually do 47 button presses. No wonder they abbreviated.
+
+For this assignment, write a module that can calculate the amount of button presses required for any phrase. Punctuation can be ignored for this exercise. Likewise, you can assume the phone doesn't distinguish between upper/lowercase characters (but you should allow your module to accept input in either for convenience).
+'''
+
+def presses(phrase):
+    phrase = phrase.upper()
+    presses = 0;
+    for letter in phrase:
+        if letter in ['A','D','G','J','M','P','T','W',' ', '1','#','*']:
+            presses += 1
+        elif letter in ['B','E','H','K','N','Q','U','X', '0']:
+            presses += 2
+        elif letter in ['C','F','I','L','O','R','V','Y']:
+            presses += 3
+        elif letter in ['S', 'Z', '2', '3', '4', '5', '6', '8']:
+            presses += 4
+        elif letter in ['7','9']:
+            presses += 5    
+    return presses
