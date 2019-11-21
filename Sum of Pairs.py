5c7d62f9189a469dd48a432393951068cc004641
Alex van Stekelenborg <poesidom@gmail.com>
2019-11-21 17:36:15 -0500

    Create Sum of Pairs.py

---
+++ Sum of Pairs.py
@@ -0,0 +1,31 @@
+#Sum of Pairs
+#Level: 5 kyu
+'''
+Given a list of integers and a single sum value, 
+return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+'''
+
+
+#this is not good code, bad because o(n^2)
+
+def sum_pairs(ints, s):
+    out = [None] * 2
+    diff = len(ints)
+    for i, val in enumerate(ints):
+        for x, val2 in enumerate(ints):    
+            if x > i and val + val2 == s and diff > (abs(x - i)):
+                diff = abs(x - i)
+                out[0] = val
+                out[1] = val2
+    
+    return None if None in out else out
+    
+    
+    
+    #solution code
+ 
+ def sum_pairs(ints, s):
+    d = set()
+    for n in ints:
+      if n in d: return [s - n, n]
+      d.add(s - n)
