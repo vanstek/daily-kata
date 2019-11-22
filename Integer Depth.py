f0230c35c3aa13a083891c221d2fa3ea0cd07af2
Alex van Stekelenborg <poesidom@gmail.com>
2019-11-22 17:09:22 -0500

    Create Integer Depth.py

---
+++ Integer Depth.py
@@ -0,0 +1,17 @@
+#Integer Depth
+#Level: 6 kyu
+'''
+The depth of an integer n is defined to be how many multiples of n it is necessary to 
+compute before all 10 digits have appeared at least once in some multiple.
+'''
+
+def compute_depth(n):
+    digits = [1,2,3,4,5,6,7,8,9,0]
+    i = 0
+    while digits:
+        i += 1
+        multiple = i * n
+        for x in str(multiple):
+            if int(x) in digits:                 
+                digits.remove(int(x)) 
+    return i
