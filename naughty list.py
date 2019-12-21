8a27ef4af1cf089a3d96e43f96d2053ad1e0d28f
Alex van Stekelenborg <poesidom@gmail.com>
2019-12-21 17:52:27 -0500

    Create naughty list.py

---
+++ naughty list.py
@@ -0,0 +1,13 @@
+#Santa's Naughty List
+#Level: 7 kyu
+'''
+Christmas is coming, and Santa has a long list to go through, to find who deserves presents 
+for the big day. Go through a list of children, and return a list containing every child who 
+appeared on Santa's list. Do not add any child more than once. Output should be sorted.
+
+Comparison should be case sensitive and the returned list should contain only one copy of 
+each name: "Sam" and "sam" are different, but "sAm" and "sAm" are not.
+'''
+
+def find_children(santas_list, children):
+    return sorted(list(set(santas_list).intersection(set(children))))
