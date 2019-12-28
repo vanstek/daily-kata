c219a896d95e88f150adeecb8564319ac319ad9e
Alex van Stekelenborg <poesidom@gmail.com>
2019-12-28 17:51:59 -0500

    Create Isograms.py

---
+++ Isograms.py
@@ -0,0 +1,10 @@
+#Isograms
+#Level: 7 kyu
+'''
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a 
+string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+'''
+def is_isogram(string):
+    for x in string.upper():
+        if string.upper().count(x) > 1: return False
+    return True
